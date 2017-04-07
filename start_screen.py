3496936e3c4ed1c940c0bb4a53741a344a1db1d3
Dominic Motuka <dominic.motuka@gmail.com>
2017-04-07 15:04:10 +0300

    Fix syntax errors

--- start_screen.py
+++ start_screen.py
@@ -27,21 +27,22 @@ def start_screen():
 
 #start_screen()
 skills = {
-        "Skill 1" : "Version Control"
-        "Skill 2" : "Agile Methodology"
-        "Skill 3" : "Programming Logic"
-        "Skill 4" : "Object Oriented Programming"
-        "Skill 5" : "Test-Driven Development"
-        "Skill 6" : "Databases"
-        "Skill 7" : "HTTP and Web Services"
-        "Skill 8" : "Front-End Development"
-        "Skill 9" : "Growth Mindset"
-        "Skill 10" : "Relationship Building"
-        "Skill 11" : "Asks Questions"
-        "Skill 12" : "Motivation and Commitment"
-        "Skill 13" : "Adaptability"
-        "Skill 14" : "Seeks Feedback"
-        "Skill 15" : "Speaking to be Understood"
-        "Skill 16" : "Writing Professionally"
-        "Skill 17" : "Git"
+        "Skill 1" : "Version Control",
+        "Skill 2" : "Agile Methodology",
+        "Skill 3" : "Programming Logic",
+        "Skill 4" : "Object Oriented Programming",
+        "Skill 5" : "Test-Driven Development",
+        "Skill 6" : "Databases",
+        "Skill 7" : "HTTP and Web Services",
+        "Skill 8" : "Front-End Development",
+        "Skill 9" : "Growth Mindset",
+        "Skill 10" : "Relationship Building",
+        "Skill 11" : "Asks Questions",
+        "Skill 12" : "Motivation and Commitment",
+        "Skill 13" : "Adaptability",
+        "Skill 14" : "Seeks Feedback",
+        "Skill 15" : "Speaking to be Understood",
+        "Skill 16" : "Writing Professionally",
+        "Skill 17" : "Git",
 }
+start_screen()
